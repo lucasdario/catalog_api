70384050eae3d54a76a7d2f4db39729f6c94582c
Lucas Dario <lucas.edu.u@hotmail.com>
2021-02-09 10:41:46 -0300

    Adicionado base_dao e pool_dao. Concluído as funções de resource em base_resource e pool_resource, agora estão integradas com a base de dados.

--- backend/resources/pool_resources.py
+++ backend/resources/pool_resources.py
@@ -1,18 +1,35 @@
+from backend.models.pool import Pool
+from backend.dao.pool_dao import PoolDao
+from flask_restful import fields, marshal_with
 from backend.resources.base_resources import BaseResource
 
 
 class PoolResources(BaseResource):
+    fields = {
+        "id": fields.Integer,
+        "fk_product": fields.Integer,
+        "fk_seller": fields.Integer,
+        "fk_category": fields.Integer,
+        "approved": fields.Boolean
+    }
+
     def __init__(self) -> None:
-        super().__init__()
+        self.__dao = PoolDao()
+        self.__model_type = Pool
+        super().__init__(self.__dao, self.__model_type)
 
-    def get(self):
-        return super().get()
+    @marshal_with(fields)
+    def get(self, id=None):
+        return super().get(id)
 
+    @marshal_with(fields)
     def post(self):
         return super().post()
 
-    def put(self):
-        return super().put()
+    @marshal_with(fields)
+    def put(self, id: int):
+        return super().put(id)
 
-    def delete(self):
-        return super().delete()
+    @marshal_with(fields)
+    def delete(self, id: int):
+        return super().delete(id)
